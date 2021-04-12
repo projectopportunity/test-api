5c0c0458987a6ce5eaf365dc70ac68f55cab388f
tpo-aeclarke <aeclarke@travelers.com>
2021-04-12 12:04:13 -0400

    woot

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const server = express()
 
 server.use(express.json())
+server.post('/api/users', require('@api/routes/users'))
 
 const PORT = process.env.PORT || 3000
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
