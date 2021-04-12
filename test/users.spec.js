609b73611d37df1a1bdb82b740849773fca8f80b
Adam Allen <aallen6@travelers.com>
2021-04-12 11:46:45 -0400

    create server

--- test/users.spec.js
+++ test/users.spec.js
@@ -1,8 +1,24 @@
 /* eslint-env mocha */
 const { expect } = require('chai')
 const axios = require('axios')
+const Procmonrest = require('procmonrest')
+const path = require('path')
 
 describe('users route', () => {
+  const serverProcess = new Procmonrest({
+    waitFor: /listening/,
+    saveLogTo: path.join(__dirname, 'users.log')
+  })
+
+  before(() => {
+    return serverProcess.start()
+  })
+  after(() => {
+    if (serverProcess.isRunning) {
+      return serverProcess.stop()
+    }
+  })
+
   context('when user is created', () => {
     describe('the response', () => {
       let res
@@ -16,7 +32,8 @@ describe('users route', () => {
         }
         return axios.post('http://localhost:3000/users', data, { validateStatus: false })
           .then((response) => {
-            res = response})
+            res = response
+          })
       })
 
       it('must have correct status code', () => {
