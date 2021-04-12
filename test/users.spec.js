191407b905b99b27a2dbf0294d0e473f0a74dc8b
Maples <CMAPLES@travelers.com>
2021-04-12 11:29:41 -0400

    first failing test

--- test/users.spec.js
+++ test/users.spec.js
@@ -1,18 +1,29 @@
 /* eslint-env mocha */
-const axios = require ('axios')
+const { expect } = require('chai')
+const axios = require('axios')
 
-describe ('users route', () => {
-  context ("when user is created", () =>{
-    describe ('the response', () => {
+describe('users route', () => {
+  context('when user is created', () => {
+    describe('the response', () => {
+      let res
+
       before(() => {
-        return axios.post('http://localhost:3000/users')
+        const data = {
+          firstName: 'test',
+          lastName: 'test',
+          emailAddress: 'test@example.com',
+          password: 'test'
+        }
+        return axios.post('http://localhost:3000/users', data, { validateStatus: false })
+          .then((response) => {
+            res = response})
       })
-      it ('must have correct status code', () => {
+
+      it('must have correct status code', () => {
         const expected = 201
-
+        const actual = res.status
+        expect(actual).to.equal(expected)
       })
-
     })
   })
-}) 
-
+})
