5a04e214f835151a2ec784d0445de15a999e94d1
Maples <CMAPLES@travelers.com>
2021-04-14 11:45:26 -0400

    Update server.js

--- app/server.js
+++ app/server.js
@@ -5,5 +5,15 @@ const server = express()
 server.use(express.json())
 server.post('/api/users', require('@api/routes/users'))
 
+server.use((err, req, res, next) => {
+  console.error(err)
+})
+
 const PORT = process.env.PORT || 3000
+
+server.once('error', (err) => {
+  console.error(err)
+  process.exit(1)
+})
+
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
